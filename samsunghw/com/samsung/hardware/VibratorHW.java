d7fe0d187ed4fb2fcd60831c96f5c638238effed
Christopher A. Williamson <you@example.com>
2019-08-19 18:23:48 +0100

    Removed some more Lineage ballocks.

--- samsunghw/com/samsung/hardware/VibratorHW.java
+++ samsunghw/com/samsung/hardware/VibratorHW.java
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-package org.lineageos.hardware;
+package com.samsung.hardware;
 
-import org.lineageos.internal.util.FileUtils;
+import comn.samsung.internal.util.FileUtils;
 
 public class VibratorHW {
 
